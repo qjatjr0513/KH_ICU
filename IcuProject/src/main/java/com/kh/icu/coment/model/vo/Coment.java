e1831fb997651c183094e0743b3740261773149e
이상엽 <115914261+Lee-sang-yeop@users.noreply.github.com>
2023-01-13 21:26:52 +0900

    다시푸시
    다시푸시 코멘트 저장안하고 푸시함

--- IcuProject/src/main/java/com/kh/icu/coment/model/vo/Coment.java
+++ IcuProject/src/main/java/com/kh/icu/coment/model/vo/Coment.java
@@ -1,5 +1,16 @@
 package com.kh.icu.coment.model.vo;
 
-public class Coment {
+import java.sql.Date;
 
+import lombok.Data;
+
+@Data
+public class Coment {
+	private int cmtNo;
+	private int cmtWriter;
+	private int conNo;
+	private String cmtContent;
+	private int cmtStar;
+	private Date createDate;
+	private String status;
 }
